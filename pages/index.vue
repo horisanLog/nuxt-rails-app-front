76e6b9ce9bd6e5fbfe88c5fe01f155861bc880e4
horisanLog <ttie7374@gmail.com>
2021-12-28 11:25:46 +0900

    add_axios.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <Tutorial />
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  data () {
+    return {
+      msgs: [] as string[]
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/borrower/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
 })
 </script>
