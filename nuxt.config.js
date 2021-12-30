111683a4fc1a75b30c8d980cd6c806dde873e1af
horisanLog <ttie7374@gmail.com>
2021-12-30 11:42:01 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,16 +85,6 @@ export default {
           grid: true
         }
       }
-    },
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
     }
   }
 }
