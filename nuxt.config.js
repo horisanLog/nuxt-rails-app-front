9565f9ef2d7b401a65511aebc65dc44ffd58c48d
horisanLog <ttie7374@gmail.com>
2021-12-30 10:44:05 +0900

    @nuxtjs/i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,13 +42,37 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/i18n'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/'
+  },
+
+  // Doc: https://nuxt-community.github.io/nuxt-i18n/basic-usage.html#nuxt-link
+  i18n: {
+    // 対応言語の指定
+    locales: ['ja', 'en'],
+    // デフォルトで使用する言語を指定
+    defaultLocale: 'ja',
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      fallbackLocale: 'ja',
+      // i18nの健康を完全に表示しない(default: false)
+      // silentTranslationWarn: true,
+
+      // フォールバック時に傾向句を発生させる
+      // trueだと警告を発生させない
+      silentFallbackWarn: true,
+      // 翻訳データ
+      messages: {
+        ja: require('./locales/ja.json'),
+        en: require('./locales/en.json')
+      }
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h2 class="hoge">
-      Borrowersテーブルの取得
-    </h2>
+    <h2 class="hoge">Borrowersテーブルの取得</h2>
     <table v-if="borrowers.length" class="users-table">
       <thead class="head">
         <tr class="row">
@@ -13,10 +11,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="(borrower, i) in borrowers"
-          :key="`borrower-${i}`"
-        >
+        <tr v-for="(borrower, i) in borrowers" :key="`borrower-${i}`">
           <td>{{ borrower.id }}</td>
           <td>{{ borrower.name }}</td>
           <td>{{ borrower.email }}</td>
@@ -25,30 +20,49 @@
       </tbody>
     </table>
 
-    <div v-else>
-      ユーザーが取得できませんでした
-    </div>
+    <div v-else>ユーザーが取得できませんでした</div>
+
+    <h2 class="hoge">nuxt-i18nの検証</h2>
+    <table v-if="borrowers.length" class="users-table">
+      <thead class="head">
+        <th>en</th>
+        <th>ja</th>
+      </thead>
+      <tbody>
+        <tr v-for="(path, i) in ['signup', 'login']" :key="`path-${i}`">
+          <td>{{ path }}</td>
+          <td>{{ $t(`title.${path}`) }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
   async asyncData({ app }) {
-    const borrowers = await app.$axios.$get('/api/borrower/borrowers')
-    return { borrowers }
+    const borrowers = await app.$axios.$get("/api/borrower/borrowers");
+    return { borrowers };
   },
   // 算出プロパティ
   computed: {
-    dateFormat () {
+    dateFormat() {
       return (date: Date) => {
-        const dateTimeFormat = new Intl.DateTimeFormat(
-          'ja', { dateStyle: 'medium', timeStyle: 'short' }
-        )
-        return dateTimeFormat.format(new Date(date))
-      }
-    }
-  }
-})
+        const dateTimeFormat = new Intl.DateTimeFormat("ja", {
+          dateStyle: "medium",
+          timeStyle: "short",
+        });
+        return dateTimeFormat.format(new Date(date));
+      };
+    },
+  },
+});
 </script>
+
+<style scoped>
+.users-table {
+  border: 2px solid blue;
+}
+</style>
