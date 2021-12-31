a02a0297a8f2fa1d3a83144fd2c200ca61fcf117
horisanLog <ttie7374@gmail.com>
2021-12-31 23:39:56 +0900

    lint

--- components/atoms/HeadingH2.stories.js
+++ components/atoms/HeadingH2.stories.js
@@ -1,13 +1,13 @@
 // Viewをインポート
-import HeadingH2 from "./HeadingH2.vue";
+import HeadingH2 from './HeadingH2.vue'
 
 // Storybookで表示される内容
 export default {
-  title: "Components/atoms/HeadingH2", //ディレクトリ
-  component: "HeadingH2" //コンポーネント名
-};
+  title: 'Components/atoms/HeadingH2', //ディレクトリ
+  component: 'HeadingH2' //コンポーネント名
+}
 
-const Template = (args, { argTypes } ) => ({
+const Template = (args, { argTypes }) => ({
   components: { HeadingH2 }, //上記のコンポーネントと揃える
   props: Object.keys(argTypes),
   // templateで、下記の場合titleとclassNameに任意の値が入る
@@ -15,11 +15,11 @@ const Template = (args, { argTypes } ) => ({
 })
 
 // 1つ目のデザイン
-export const Primary = Template.bind({});
-Primary.args = { title: "見出し2です", className: "el-lv2Heading" };
-Primary.storyName = "見出し2デザイン①";
+export const Primary = Template.bind({})
+Primary.args = { title: '見出し2です', className: 'el-lv2Heading' }
+Primary.storyName = '見出し2デザイン①'
 
 // 2つ目のデザイン
-export const Secondary = Template.bind({});
-Secondary.args = { title: "見出し2です", className: "el-lv2Heading-2"};
-Secondary.storyName = "見出し2デザイン②";
+export const Secondary = Template.bind({})
+Secondary.args = { title: '見出し2です', className: 'el-lv2Heading-2' }
+Secondary.storyName = '見出し2デザイン②'
