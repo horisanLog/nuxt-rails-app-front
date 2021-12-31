83f6f776c3e6aa0d675bc979e4b1ab3069ef56c0
horisanLog <ttie7374@gmail.com>
2021-12-31 22:19:54 +0900

    barの追加

--- components/organisms/HomeAppBar.vue
+++ components/organisms/HomeAppBar.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-app-bar app dark>
-    <app-logo />
+  <v-app-bar
+    app
+    :dark="!isScrollPoint"
+    :height="appBarHeight"
+    :color="toolbarStyle.color"
+    :elevation="toolbarStyle.elevation"
+  >
+    <app-logo @click.native="goTo('scroll-top')" />
     <v-toolbar-title>
       {{ appName }}
     </v-toolbar-title>
     <v-spacer />
     <v-toolbar-items class="ml-2">
-      <v-btn
-        v-for="(menu, i) in menus"
-        :key="`menu-btn-${i}`"
-        text
-      >
+      <v-btn v-for="(menu, i) in menus" :key="`menu-btn-${i}`" text @click="goTo(menu.title)">
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
@@ -23,17 +25,48 @@ import AppLogo from '~/components/ui/appLogo'
 export default {
   // data (context: { $config: { appName: "Value" } })
   components: {
-    AppLogo,
+    AppLogo
   },
   props: {
     menus: {
       type: Array,
       default: () => []
+    },
+    imgHeight: {
+      type: Number,
+      default: 0
     }
   },
-  data({ $config: { appName } }) {
+  data({ $config: { appName }, $store }) {
     return {
-      appName
+      appName,
+      scrollY: 0,
+      appBarHeight: $store.state.styles.beforeLogin.appBarHeight
+    }
+  },
+  // computed追加
+  computed: {
+    isScrollPoint() {
+      return this.scrollY > this.imgHeight - this.appBarHeight
+    },
+    toolbarStyle() {
+      const color = this.isScrollPoint ? 'white' : 'transparent'
+      const elevation = this.isScrollPoint ? 4 : 0
+      return { color, elevation }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      this.scrollY = window.scrollY
+    },
+    goTo (id) {
+      this.$vuetify.goTo(`#${id}`)
     }
   }
 }
---
+++ store/index.js
@@ -0,0 +1,22 @@
+// 変数
+export const state = () => ({
+  loggedIn: false,
+  styles: {
+    beforeLogin: {
+      appBarHeight: 56
+    }
+  }
+})
+
+// 算出プロパティ
+export const getter = {
+
+}
+
+// stateの値を変更する場所
+export const mutations = {
+
+}
+
+// メソッド
+export const actions = {}
