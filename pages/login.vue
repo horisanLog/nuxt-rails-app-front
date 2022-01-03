5605d96502df5cb5bb4e6dd000d8826d8ae3fad9
horisanLog <ttie7374@gmail.com>
2022-01-03 15:58:46 +0900

    submit

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <bef-login-form-card #user-form-card-content>
-    <v-form ref="form" v-model="isValid">
+    <v-form ref="form" v-model="isValid" @submit.prevent="login">
       <user-form-email v-model:email="params.auth.email" no-validation />
       <user-form-password v-model:password="params.auth.password" no-validation />
 
@@ -11,12 +11,12 @@
       </v-card-actions>
       <v-card-text class="px-0">
         <v-btn
+          type="submit"
           :disabled="!isValid || loading"
           :loading="loading"
           block
           color="myblue"
           class="white--text"
-          @click="login"
         >
           ログインする
         </v-btn>
@@ -37,7 +37,7 @@ export default {
     UserFormPassword
   },
   layout: 'BeforeLogin',
-  data() {
+  data({ $store }) {
     return {
       isValid: false,
       loading: false,
